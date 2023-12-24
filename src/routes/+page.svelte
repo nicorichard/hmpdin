756d288367b041e30722c2a1dbddf7f856937544
Nico Richard <nicolas.richard@justeattakeaway.com>
2023-12-24 13:28:12 -0600

    swap the form to have all options from js, add labels

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,29 @@
 	import calculate from './calculate.js';
 
 	let result = 0;
+
+    let size = 14;
     let people = 1;
 
-    let crust = "regular";
+    let crustOptions = [
+        {
+            title: "thin",
+            multiplier: 1.4,
+        },
+        {
+            title: "regular",
+            multiplier: 1,
+        }
+    ];
+    let crust = crustOptions[1];
 
-    let hungerLevels = [
+    let hungerOptions = [
         {
             title: "snack",
             value: 2.5,
         },
         {
-            title: "regular",
+            title: "meal",
             value: 3.5,
         },
         {
@@ -20,10 +32,10 @@
             value: 6,
         }
     ];
-    let hunger = hungerLevels[1];
+    let hunger = hungerOptions[1];
 
 	function handleSubmit() {
-        result = calculate(hunger.value, people, 14, crust === "thin");
+        result = calculate(hunger.value, people, size, crust.multiplier);
 	}
 
     function onChange() {
@@ -35,24 +47,42 @@
 
 
 <form on:submit|preventDefault={handleSubmit}>
-    <select
-        bind:value={hunger}
-		on:change={onChange}
-    >
-        {#each hungerLevels as level}
-            <option value={level}>{level.title}</option>
-        {/each}
-    </select>
-
-    <select
-        bind:value={crust}
-		on:change={onChange}
-    >
-        <option value="thin">thin</option>
-        <option value="regular">regular</option>
-    </select>
+    <label>
+        hunger
+        <select
+            bind:value={hunger}
+            on:change={onChange}
+        >
+            {#each hungerOptions as level}
+                <option value={level}>{level.title}</option>
+            {/each}
+        </select>
+    </label>
 
     <label>
+        crust
+        <select
+            bind:value={crust}
+            on:change={onChange}
+        >
+            {#each crustOptions as option}
+                <option value={option}>{option.title}</option>
+            {/each}
+        </select>
+    </label>
+
+    <label>
+        size
+        <input
+            bind:value={size}
+            on:change={onChange}
+            type="number"
+            min="1"
+        />
+    </label>
+
+    <label>
+        people
         <input
             bind:value={people}
             on:change={onChange}
--- src/routes/calculate.js
+++ src/routes/calculate.js
@@ -9,21 +9,20 @@ const standardPizzaVolume = circleArea(14);
 
 /**
  * @param {number} diameter
- * @param {boolean} thinCrust
+ * @param {number} crustMultiplier
  */
-function volumeMultiplier(diameter, thinCrust) {
-    const thinCrustMultiplier = thinCrust ? 1.4 : 1;
-    return standardPizzaVolume / circleArea(diameter) * thinCrustMultiplier;
+function volumeMultiplier(diameter, crustMultiplier) {
+    return standardPizzaVolume / circleArea(diameter) * crustMultiplier;
 }
 
 /**
  * @param {number} hungerLevel
  * @param {number} people
  * @param {number} diameter
- * @param {boolean} thinCrust
+ * @param {number} crustMultiplier
  */
-export default function calculate(hungerLevel, people, diameter, thinCrust) {
-    const volumeMod = volumeMultiplier(diameter, thinCrust);
+export default function calculate(hungerLevel, people, diameter, crustMultiplier) {
+    const volumeMod = volumeMultiplier(diameter, crustMultiplier);
     const floatResult = (hungerLevel * people) / 8 * volumeMod;
 
     return Math.ceil(floatResult);
